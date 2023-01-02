5449940ddbd602496293850590003e5b9cab64ad
l1ghtm00n <lizardfrogdamn@google.com>
2023-01-02 22:08:50 +0300

    fix: fixed linter errors

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -265,7 +265,7 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/*ccn*/) {
+function isCreditCardNumber(/* ccn */) {
   throw new Error('Not implemented');
 }
 
